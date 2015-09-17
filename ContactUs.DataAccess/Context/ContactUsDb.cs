07f7b488cdfb234dc4255f72ae4739a84f7158e4
Rapeepat <rapeepatph@aerothai.co.th>
2015-09-17 10:40:52 +0700

    complete Structure

---
+++ ContactUs.DataAccess/Context/ContactUsDb.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data.Entity;
+using ContactUs.Models;
+
+namespace ContactUs.DataAccess.Context
+{
+    public class ContactUsDb : DbContext
+    {
+        public DbSet<Ticket> Tickets { get; set; }
+    }
+   
+}
